cede970b26dd5fc0c1ac42343bd45306dc0ba058
Bridget <brturnbul@gmail.com>
2018-06-13 18:03:09 +0100

    minor tweaks to portfolio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 $(() =>{
 
-var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+  $('.work').slick({
+    dots: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
 
 });
